e7e98e14aa0c26726009ec1b3025d3cd162ed962
ali-b13 <aliiskran004@gmail.com>
2024-05-05 21:00:35 +0300

    fixed google provider

--- components/Feed.tsx
+++ components/Feed.tsx
@@ -82,6 +82,7 @@ const Feed = () => {
       data={prompts}
       handleTagClick={handleTagClick}
       />
+    
 		</>
 	);
 };
---
+++ components/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className=' bg-slate-400 h-20 w-full flex justify-center items-center fixed bottom-0 '>
+       <div className='text-white font-mono'>
+          Created By Ali Alamri  
+       </div>
+    </div>
+  )
+}
+
+export default Footer
